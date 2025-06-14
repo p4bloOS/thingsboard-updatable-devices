f478dcd3840e7b15d7669ec18386cc0b39b647e0
pablopy <pgimher@upv.edu.es>
2025-06-14 13:53:50 +0200

    Reestructuración

--- 02-basic_ota_update/devices/my-esp32-device/main.py
+++
@@ -1,9 +0,0 @@
-import thingsboard_ota_links
-
-print("Hola desde main.py")
-
-import thing
-if hasattr(thing, "main"):
-    main_function = getattr(thing, "main")
-    if callable(main_function):
-        thing.main()
