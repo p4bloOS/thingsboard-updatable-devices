146ffe12a7850bb0e5c3222dd8479042d9145a6a
pablopy <pgimher@upv.edu.es>
2025-07-15 19:34:11 +0200

    asyncio y funciones varias en paralelo

--- devices/micropython/src/main.py
+++ devices/micropython/src/main.py
@@ -3,6 +3,8 @@ import machine
 import random
 import json
 import utils
+import asyncio
+import gc
 
 # Loggers
 log = utils.get_custom_logger("main")
@@ -12,42 +14,70 @@ utils.get_custom_logger("ota_helper")
 client = utils.get_updatable_thingsboard_client()
 
 
-def publish_random_value():
+async def memory_report(period_ms):
     """
-    Envía a la plataforma un dato de telemetría con clave "random_value" y como
-    valor un entero aleatorio entre 0 y 8.
+    Envía telemetría a la plataforma conn datos sobre la memoria del heap
+    asignada y libre.
     """
-    random_value = random.randint(0,8)
-    telemetry = {"random_value" : random_value}
-    log.debug("Enviando valor random")
-    client.send_telemetry(telemetry)
-    log.info("Telemetría enviada enviada: %s)" % json.dumps(telemetry))
+    while True:
+        mem_free = gc.mem_free()
+        mem_alloc = gc.mem_alloc()
+        telemetry = {"memory_free" : mem_free, "memory_allocated": mem_alloc}
+        client.send_telemetry(telemetry)
+        await asyncio.sleep_ms(period_ms)
 
 
-def on_attributes_change(result, exception):
-    if exception is not None:
-        log.error(f"Exception: {str(exception)}")
-    else:
-        log.info(f"Se ha actualizado un atributo: {result}")
-
-
-if __name__ == "__main__":
-
-    log.info("Iniciando programa principal")
-    client.connect()
-    log.info("Conexión establecida con la plataforma Thingsboard")
-
-    # client.subscribe_to_all_attributes(on_attributes_change)
+async def heartbeat_LED():
 
     led_pin = machine.Pin(2, machine.Pin.OUT)
     led_pin.off()
+    while True:
+        led_pin.on()
+        await asyncio.sleep_ms(1700)
+        led_pin.off()
+        await asyncio.sleep_ms(100)
+        led_pin.on()
+        await asyncio.sleep_ms(100)
+        led_pin.off()
+        await asyncio.sleep_ms(100)
 
+
+async def listen_thingsboard(period_ms):
+
+    def on_attributes_change(result, exception):
+        if exception is not None:
+            log.error(f"Exception: {str(exception)}")
+        else:
+            log.info(f"Se ha actualizado un atributo: {result}")
+
+    def on_server_side_rpc_request(request_id, request_body):
+        print(request_id, request_body)
+        if request_body["method"] == "garbage_collection":
+            gc.collect()
+            client._client.publish(
+                f"v1/devices/me/rpc/response/{request_id}",
+                json.dumps({"Garbage collection": "Done"}),
+                qos=client.quality_of_service
+            )
+
+    client.subscribe_to_all_attributes(on_attributes_change)
+    client.set_server_side_rpc_request_handler(on_server_side_rpc_request)
 
     while True:
+        client._client.check_msg()
+        await asyncio.sleep_ms(period_ms)
 
-        # Led signal
-        led_pin.on()
-        # time.sleep_ms(500)
-        led_pin.off()
 
-        client.wait_for_msg()
+async def main():
+    await asyncio.gather(
+        heartbeat_LED(),
+        memory_report(1_000),
+        listen_thingsboard(1_000)
+    )
+
+
+if __name__ == "__main__":
+    log.info("Iniciando programa principal")
+    client.connect()
+    log.info("Conexión establecida con la plataforma Thingsboard")
+    asyncio.run(main())
